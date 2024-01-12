1605d6bb55d86570cb8a02ba3bab07a3fc5ecb9d
Zihao Bai <zbai29@wisc.edu>
2024-01-13 06:08:12 +0800

    Make FeynWann-Current use a.u. time

--- FeynWann-Current.py
+++ FeynWann-Current.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+fs  = 41.341373335# fs to a.u.
 jx_elec_tot=pd.DataFrame()
 jy_elec_tot=pd.DataFrame()
 jz_elec_tot=pd.DataFrame()
@@ -15,13 +16,13 @@ with open('lindbladLinear.out') as f:
             currentx.append(line.split()[2])
             currenty.append(line.split()[3])
             currentz.append(line.split()[4])
-            
-jx_elec_tot['time']=time
-jx_elec_tot['j(t)']=currentx
+time=np.array(time)*fs
+jx_elec_tot['time(a.u.)']=time
+jx_elec_tot['j(t)(A/cm^2)']=currentx
 jy_elec_tot['time']=time
-jy_elec_tot['j(t)']=currenty
+jy_elec_tot['j(t)(A/cm^2)']=currenty
 jz_elec_tot['time']=time
-jz_elec_tot['j(t)']=currentz
+jz_elec_tot['j(t)(A/cm^2)']=currentz
 
 
 jx_elec_tot.to_csv('jx_elec_tot.out',sep='\t',index=False)
