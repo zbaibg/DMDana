e236ae3acc854fde6ecbbf469ed69faf33e3c0ad
Zihao Bai <zbai29@wisc.edu>
2024-01-19 23:05:54 -0600

    Add max time for occup relevant scripts and add ValueError if there is no occupations_t0.out

--- occup-deriv.py
+++ occup-deriv.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python
 """_summary_
 This script plots the E-axis maximum for the time derivative of the occupation funciton f(E,t), namely (df/dt)max_in_E_axis.
-(Using second center finite difference)
+(Using second center finite difference).
+This program does not read time from file content for now. It only count the file numbers to get time. 
+So be sure that your occupation filelists include complete number of files and also occupations_t0.out
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,30 +14,48 @@ import configparser
 config = configparser.ConfigParser(inline_comment_prefixes="#")
 config.read('DMDana.ini')
 Input=config['occup-deriv']
+
 fs  = 41.341373335
 sec = 4.1341373335E+16
 #Hatree_to_eV = 27.211386245988
 timestep_for_all_files=Input.getint('timestep_for_occupation_output') #fs
 filelist_step=Input.getint('filelist_step') # select parts of the filelist
-timestep_for_selected_file=timestep_for_all_files*filelist_step
+timestep_for_selected_file=timestep_for_all_files*filelist_step#fs
 # Read all the occupations file names at once
+if glob.glob('occupations_t0.out')==[]:
+    raise ValueError("Did not found occupations_t0.out")
 occup_scat_files = glob.glob('occupations_t0.out')+sorted(glob.glob('occupations-*out'))
 # Select partial files
 occup_scat_files=occup_scat_files[::filelist_step]
 
-
-n = len(occup_scat_files)
-data= np.full((n, 5646), np.nan)
+t_tot = Input.getint('t_max') # fs
+if t_tot<=0:
+    maxmium_file_number_plotted_exclude_t0=len(occup_scat_files)-1
+else:
+    
+    maxmium_file_number_plotted_exclude_t0=int(round(t_tot/timestep_for_selected_file))
+    if maxmium_file_number_plotted_exclude_t0>len(occup_scat_files)-1:
+        raise ValueError('t_tot is larger than maximum time of data we have.')
+    occup_scat_files=occup_scat_files[:maxmium_file_number_plotted_exclude_t0+2]
+    # keep the first few items of the filelists in need to avoid needless calculation cost.
+    # If len(occup_scat_files)-1 > maxmium_file_number_plotted_exclude_t0.
+    # I Add one more points larger than points needed to be plotted for later second-order difference 
+    # calculation. However if maxmium_file_number_plotted_exclude_t0 happens to be len(occup_scat_files)-1, 
+    # the number of points considered in second-order difference calculation is still the number of points 
+    # to be plotted
+t_tot=maxmium_file_number_plotted_exclude_t0*timestep_for_selected_file#fs
+n = len(occup_scat_files)#number of files left in "occup_scat_files"
+data= np.full((n, 5646), np.nan)# why 5646?
 dfdt = np.full((n,5646), np.nan)
 dfdtMax = np.full((n), np.nan)
-tarray=np.array(range(n))*timestep_for_selected_file/1000
+tarray=np.array(range(n))*timestep_for_selected_file/1000#ps
 for ind in range(n):
     data[ind,:] = np.loadtxt(occup_scat_files[ind])[:,1]
-dfdt=np.gradient(data,timestep_for_selected_file,axis=0)
+dfdt=np.gradient(data,timestep_for_selected_file,axis=0)#df/fs
 dfdtMax = np.max(dfdt,axis=1)
 fig4, ax4 = plt.subplots(figsize=(10, 8))
-ax4.plot(tarray, dfdtMax, '--.')
+ax4.plot(tarray[0:maxmium_file_number_plotted_exclude_t0+1], dfdtMax[0:maxmium_file_number_plotted_exclude_t0+1], '--.')
 ax4.set_yscale('log')
 ax4.set_xlabel('t (ps)')
-ax4.set_ylabel(r'max$[\frac{df}{dt}]$')
-fig4.savefig('dfdt-max.png')
+ax4.set_ylabel(r'max$[\frac{df}{dt}]$ unit:df/fs')
+fig4.savefig('dfdt_max_Ttot%dfs_Step%dfs.png'%(t_tot,timestep_for_selected_file))
--- occup-time.py
+++ occup-time.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 """_summary_
 This script plot the occupation functions with time, f(E,t). It also plots the E-axis maximum for the time derivative of f(E,t), (df/dt)max_in_E_axis.
+This program does not read time from file content for now. It only count the file numbers to get time. 
+So be sure that your occupation filelists include complete number of files and also occupations_t0.out
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,14 +19,15 @@ sec = 4.1341373335E+16
 Hatree_to_eV = 27.211386245988
 
 timestep_for_all_files=Input.getint('timestep_for_occupation_output') #fs
-t_tot = Input.getint('t_max') # fs
 filelist_step=Input.getint('filelist_step') # select parts of the filelist
-
 timestep_for_selected_file=timestep_for_all_files*filelist_step/1000 #ps
 # Read all the occupations file names at once
 occup_scat_files = glob.glob('occupations_t0.out')+sorted(glob.glob('occupations-*out'))
 # Select partial files
 occup_scat_files=occup_scat_files[::filelist_step]
+t_tot = Input.getint('t_max') # fs
+if t_tot<=0:
+    t_tot= (len(occup_scat_files)-1)*timestep_for_selected_file*1000
 # Read and plot data from one file at time 
 fig = plt.figure(figsize=(12, 10))
 ax =fig.add_subplot(111,projection='3d')
@@ -45,5 +48,5 @@ ax.yaxis.set_pane_color('w')
 ax.zaxis.set_pane_color('w')
 
 ax.view_init(elev=20., azim=-70, roll=0)
-fig.savefig('occup_time_Ttot%d_Step%d.png'%(t_tot,timestep_for_selected_file*1000), bbox_inches="tight")
+fig.savefig('occup_time_Ttot%dfs_Step%dfs.png'%(t_tot,timestep_for_selected_file*1000), bbox_inches="tight")
 
