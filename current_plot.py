cdc2a4aca7b24d270ed132364e732c75c98e870b
zbaibg <zbai29@wisc.edu>
2024-02-17 23:51:53 -0600

    add t_min and t_max for current-plot

--- current_plot.py
+++ current_plot.py
@@ -5,9 +5,14 @@ This script plots the current figures
 
 import matplotlib.pyplot as plt
 from constant import *
+import numpy as np
 from global_variable import config
+tmax=config.Input.get("tmax",-1)
+if tmax==-1:
+    tmax=np.max(config.jx_data[:,0])/fs
+tmin=config.Input.get("tmin",0)
+current_plot_output=config.Input.get('current_plot_output',"j.png")
 def do():
-    current_plot_output=config.Input['current_plot_output']
     #Plot Current
     if not config.only_jtot:
         fig1, ax1 = plt.subplots(3,3, figsize=(10,6),dpi=200,sharex=True)
@@ -16,6 +21,8 @@ def do():
             for i in range(3):
                 ax1[i][j].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
                 ax1[i][j].yaxis.major.formatter._useMathText = True 
+                total_time=tmax-tmin 
+                ax1[i][j].set_xlim(tmin-0.05*total_time,tmax+0.05*total_time)
             ax1[0][0].set_ylabel('$j^{tot}(t)$ A/cm$^2$')
             ax1[1][0].set_ylabel('$j^{diag}(t)$ A/cm$^2$')
             ax1[2][0].set_ylabel('$j^{off-diag}(t)$ A/cm$^2$')
@@ -37,6 +44,7 @@ def do():
             ax1[j].set_title(jdirection)
             ax1[j].set_xlabel('t (fs)')  
             ax1[j].plot(jtemp[:,0]/fs, jtemp[:,1])#, label='$j'+jdirection+'^{tot}(t)$    polarization = '+light_label)
+            ax1[j].set_xlim(tmin-0.05*total_time,tmax+0.05*total_time)
         fig1.tight_layout()
         fig1.savefig(current_plot_output)
         plt.close(fig1)
