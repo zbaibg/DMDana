decea3ceadcc835dacc9d68bc833471a2ba41d36
zbaibg <zbai29@wisc.edu>
2024-02-21 21:54:39 -0600

    Plot and use the True Fermi Occupation function at t0. Change the occup_time figure output name format a little bit ( from %d to %.1f)

--- occup_time.py
+++ occup_time.py
@@ -26,6 +26,8 @@ occup_timestep_for_selected_file_ps=config.occup_timestep_for_selected_file_ps
 cmap = mpl.colormaps['rainbow']
 figsize=(12, 10)#Width, height in inches.
 dpi=100.0#The resolution of the figure in dots-per-inch.
+mu_au=config.mu_au
+temperature_au=config.temperature_au
 
 class plot_occup:
     def __init__(self,figure_style_this,Substract_initial_occupation_this):
@@ -34,8 +36,8 @@ class plot_occup:
         self.fig = None
         if(self.figure_style_this not in ['3D','heatmap'] ):
             raise ValueError('figure_style should be 3D or heatmap')
-        self.occupation_min_for_alldata=None
-        self.occupation_max_for_alldata=None
+        self.occupation_min_for_alldata=0#Must be 0, would be updated later
+        self.occupation_max_for_alldata=0#Must be 0, would be updated later
         self.figtemp=None # A temp file used for create colorbar
     def do(self):
         self.fig = plt.figure(figsize=figsize,dpi=dpi)
@@ -55,34 +57,48 @@ class plot_occup:
             self.ax.set_xlim(occup_time_plot_lowE, occup_time_plot_highE)
         self.ax.set_xlabel('E (eV)')
         self.fig.colorbar(self.figtemp,label='Time (ps)')    
+    def fermi(self,temperature_au,mu_au,elist):
+        ebyt = (elist - mu_au) / temperature_au
+        occup=np.zeros(len(elist))
+        occup[ebyt < -46]=1
+        occup[ebyt > 46]=0
+        index_middle=np.logical_and(ebyt >= -46, ebyt <= 46)
+        occup[index_middle] = 1. / (np.exp(ebyt[index_middle]) + 1)
+        return occup
     
+    def read_occup_file(self,filename):
+        with open(filename) as f:
+            firstline_this_file=f.readline()
+            time_this_file_fs=float(firstline_this_file.split()[12])/fs 
+            #nstep=firstline.split()[9]
+        data = np.loadtxt(filename)
+        return time_this_file_fs,data
+            
+    def plot_one_file(self,time_this_file_fs,data):
+        data=np.array(data)
+        if self.Substract_initial_occupation_this:
+            data[:,1]=data[:,1]-self.data_first[:,1]
+        if occup_time_plot_set_Erange:
+            #data=data[data[:,0].argsort()]
+            data=data[np.logical_and(data[:,0]>occup_time_plot_lowE/Hatree_to_eV,data[:,0]<occup_time_plot_highE/Hatree_to_eV)]
+        self.figtemp=self.plot_data_function(data,time_this_file_fs)            
+        self.occupation_max_for_alldata=max(self.occupation_max_for_alldata,np.max(data[:,1]))
+        self.occupation_min_for_alldata=min(self.occupation_min_for_alldata,np.min(data[:,1]))
     def plot_data(self,plot_data_function):
-        temp_occupation_max_for_alldata=0
-        temp_occupation_min_for_alldata=0
-        #note that t in occupations_t0.out is actually not 0
-        #we just take it as 0 here.
-        data_first=np.loadtxt('occupations_t0.out')
-        for index, file in enumerate(occup_selected_files):
-            with open(file) as f:
-                firstline_this_file=f.readline()
-                time_this_file_fs=float(firstline_this_file.split()[12])/fs 
-                #nstep=firstline.split()[9]
+        self.plot_data_function=plot_data_function
+        
+        #Plot the fermi function at t=0
+        data_temp=np.loadtxt('occupations_t0.out')
+        self.data_first=np.array(data_temp)
+        self.data_first[:,1]=self.fermi(temperature_au,mu_au,data_temp[:,0])
+        self.plot_one_file(time_this_file_fs=0,data=self.data_first)
+        #Plot all the other files
+        for file in occup_selected_files:
+            time_this_file_fs,data=self.read_occup_file(file)
             if time_this_file_fs>occup_t_tot:
                 break
-            data = np.loadtxt(file)
-            #if index==0:
-                #data_first=np.array(data)
-            if self.Substract_initial_occupation_this:
-                data[:,1]=data[:,1]-data_first[:,1]
-            if occup_time_plot_set_Erange:
-                #data=data[data[:,0].argsort()]
-                data=data[np.logical_and(data[:,0]>occup_time_plot_lowE/Hatree_to_eV,data[:,0]<occup_time_plot_highE/Hatree_to_eV)]
-            figtemp=plot_data_function(data,time_this_file_fs)            
-            temp_occupation_max_for_alldata=max(temp_occupation_max_for_alldata,np.max(data[:,1]))
-            temp_occupation_min_for_alldata=min(temp_occupation_min_for_alldata,np.min(data[:,1]))
-        self.occupation_min_for_alldata= temp_occupation_min_for_alldata
-        self.occupation_max_for_alldata= temp_occupation_max_for_alldata 
-        self.figtemp=figtemp   
+            self.plot_one_file(time_this_file_fs,data)
+
     def plot_data_func_3D(self,data,time_this_file_fs):
         return self.ax.scatter(data[:,0]*Hatree_to_eV, data[:,1], zs=time_this_file_fs/1000, zdir='y',c=[time_this_file_fs/1000]*data.shape[0],cmap=cmap, vmin=0,vmax=occup_t_tot/1000)
     
@@ -116,11 +132,13 @@ class plot_occup:
             self.ax.set_ylabel('f')        
     def save_fig(self):
         if self.Substract_initial_occupation_this:
-            self.fig.savefig('delta_occup_time_Ttot%dfs_Step%dfs_%s.png'%(occup_t_tot,occup_timestep_for_selected_file_ps*1000,self.figure_style_this), bbox_inches="tight")
+            self.fig.savefig('delta_occup_time_Ttot%.1ffs_Step%.1ffs_%s.png'%(occup_t_tot,occup_timestep_for_selected_file_ps*1000,self.figure_style_this), bbox_inches="tight")
         else:
-            self.fig.savefig('occup_time_Ttot%dfs_Step%dfs_%s.png'%(occup_t_tot,occup_timestep_for_selected_file_ps*1000,self.figure_style_this), bbox_inches="tight")   
+            self.fig.savefig('occup_time_Ttot%.1ffs_Step%.1ffs_%s.png'%(occup_t_tot,occup_timestep_for_selected_file_ps*1000,self.figure_style_this), bbox_inches="tight")   
         
 def do():
+    print('temperature(K): %.3d'%(temperature_au/Kelvin))
+    print('mu(eV): %.3d'%(mu_au/eV))
     if output_all_figure_types:
         for figure_style_each in ['3D','heatmap']:
             for Substract_initial_occupation_each in [True,False]:
