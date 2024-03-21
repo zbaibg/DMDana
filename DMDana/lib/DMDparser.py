d33c4186464b6593a5c5e306a537112105519ca5
zbaibg <zbai29@wisc.edu>
2024-03-21 00:42:17 -0500

    Add some comment for DMDparser.py

--- DMDana/lib/DMDparser.py
+++ DMDana/lib/DMDparser.py
@@ -30,6 +30,19 @@ def read_text_from_file(filepath,marklist,locationlist,stop_at_first_find):
     return resultlist
 
 def get_current_data(folder):
+    """
+    get the data of the current vs time from jx(y,z)_elec_tot.out
+
+    Parameters
+    1. path: the folder containing jx(y,z)_elec_tot.out
+
+    Return
+    the data of the current vs time for each direction.
+    The first column is time(au), the second column is current(A/cm^2).
+    1. jx_data
+    2. jy_data
+    3. jz_data
+    """
     jxpath=folder+"/jx_elec_tot.out"
     jypath=folder+"/jy_elec_tot.out"
     jzpath=folder+"/jz_elec_tot.out"
@@ -39,6 +52,15 @@ def get_current_data(folder):
     return jx_data,jy_data,jz_data
 
 def get_DMD_param(path='.'):
+    """
+    get the parameters in param.in
+
+    Parameters
+    1. path: the folder containing param.in
+
+    Return
+    1. DMDparam_value: a dictionary containing the parameters in param.in
+    """
     DMDparam_value=dict()
     filepath = check_and_get_path(path+'/param.in')
     with open(filepath) as f:
@@ -56,6 +78,16 @@ def get_DMD_param(path='.'):
     return DMDparam_value
 
 def get_mu_temperature(DMDparam_value,path='.'):
+    """
+    get mu and temperature from ldbd_data/ldbd_size.dat, param.in, and out(DMD.out)
+
+    Parameters
+    1. path: the folder containing these files
+
+    Return
+    1. mu_au
+    2. temperature_au
+    """
     # read ldbd_size.dat
     filepath = check_and_get_path(path+'/ldbd_data/ldbd_size.dat')
     mu_au_text,temperature_au_text=read_text_from_file(filepath,marklist=["mu","# T"],locationlist=[2,0],stop_at_first_find=True)
@@ -79,6 +111,22 @@ def get_mu_temperature(DMDparam_value,path='.'):
     return mu_au,temperature_au
 
 def get_erange(path='.'):
+    """
+    get different energy parameters from ldbd_data/ldbd_size.dat and ldbd_data/ldbd_erange_brange.dat
+
+    Parameters
+    1. path: the folder containing ldbd_data folder
+
+    Return
+    1.EBot_probe_au
+    2.ETop_probe_au
+    3.EBot_dm_au
+    4.ETop_dm_au
+    5.EBot_eph_au
+    6.ETop_eph_au
+    7.EvMax_au
+    8.EcMin_au
+    """
     filepath = check_and_get_path(path+'/ldbd_data/ldbd_size.dat')
     EBot_probe_au, ETop_probe_au, EBot_dm_au, ETop_dm_au, EBot_eph_au, ETop_eph_au=read_text_from_file(filepath,marklist=['# EBot_probe, ETop_probe, EBot_dm, ETop_dm, EBot_eph, ETop_eph']*6,locationlist=range(6),stop_at_first_find=True)
     filepath= check_and_get_path(path+'/ldbd_data/ldbd_erange_brange.dat')
