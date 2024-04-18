27e3b05cd223e96d36f595da367c2f303b803f33
zbaibg <zbai29@wisc.edu>
2024-04-18 13:17:26 -0500

    organize all main data and methods into "DMD" class. This is a very intuitive data class

--- DMDana/lib/DMDparser.py
+++ DMDana/lib/DMDparser.py
@@ -1,8 +1,13 @@
 import numpy as np
+from . import DMDparser
+from  pydantic import BaseModel
 import os
 from . import constant as const
 import glob
+from dataclasses import dataclass,make_dataclass,field
+from .. import do as DMDdo 
 from typing import List
+from .param_in_class import param_class
 def check_and_get_path( filepath):
     assert os.path.isfile(filepath),"%s does not exist."%filepath
     return filepath
@@ -158,3 +163,153 @@ def get_erange(path='.'):
         assert len(line)==2, "The first line of ldbd_erange_brange.dat is not correctly setted."
         EvMax_au,EcMin_au=line
     return [float(i) for i in [EBot_probe_au, ETop_probe_au, EBot_dm_au, ETop_dm_au, EBot_eph_au, ETop_eph_au ,EvMax_au, EcMin_au]]
+
+
+
+class default_class(BaseModel):
+    only_jtot:bool
+    folders:str
+class current_plot_class(default_class):
+    current_plot_output:str
+    t_min:int
+    t_max:int
+    smooth_on:bool
+    smooth_method:str
+    smooth_times:int
+    smooth_windowlen:int
+    plot_all:bool
+class FFT_DC_convergence_test_class(default_class):
+    Cutoff_step:int
+    Cutoff_min:int
+    Cutoff_max:int
+    Window_type_list:str
+    Database_output_csv:bool
+    Database_output_xlsx:bool
+    Database_output_filename_csv:str
+    Database_output_filename_xlsx:str
+    Figure_output_filename:str
+class FFT_spectrum_plot_class(default_class):
+    Cutoff_list:int
+    Window_type_list:str
+    Log_y_scale:bool
+    Summary_output_csv:bool
+    Summary_output_xlsx:bool
+    Summary_output_filename_csv:str
+    Summary_output_filename_xlsx:str
+class occup_time_class(default_class):
+    t_max:int
+    filelist_step:int
+    occup_time_plot_set_Erange:bool
+    occup_time_plot_lowE:float
+    occup_time_plot_highE:float
+    plot_conduction_valence:bool
+    plot_occupation_number_setlimit:bool
+    plot_occupation_number_min:int
+    plot_occupation_number_max:int
+    output_all_figure_types:bool
+    figure_style:str
+    fit_Boltzmann:bool
+    fit_Boltzmann_initial_guess_mu:int
+    fit_Boltzmann_initial_guess_mu_auto:bool
+    fit_Boltzmann_initial_guess_T:int
+    fit_Boltzmann_initial_guess_T_auto:bool
+    Substract_initial_occupation:bool
+class occup_deriv_class(default_class):
+    t_max:int
+    filelist_step:int
+    
+class DMDana_ini_class(BaseModel):
+   
+    section_DEFAULT:default_class
+    section_current_plot:current_plot_class
+    section_FFT_DC_convergence_test:FFT_DC_convergence_test_class
+    section_FFT_spectrum_plot:FFT_spectrum_plot_class
+    section_occup_time:occup_time_class
+    section_occup_deriv:occup_deriv_class    
+class analyze_class:
+    def __init__(self,folder:str):
+        self.folder:str=folder
+        self.config:DMDana_ini_class=None
+        self._configfile_path:str=None
+        self._DMDana_ini=None
+        self._configparser=None
+        self.use_local_config()
+    @property
+    def configfile_path(self):
+        return self._configfile_path
+    @configfile_path.setter
+    def configfile_path(self,value):
+        assert os.path.isfile(value), "%s does not exist"%value
+        self._configfile_path=value
+        self._DMDana_ini=DMDdo.config.DMDana_ini_Class(self.configfile_path)
+        self._configparser=self._DMDana_ini.DMDana_ini_configparser
+        self._configparser['DEFAULT']['folder']=self.folder
+        self._DMDana_ini.folderlist=[self.folder]
+        self.config=DMDana_ini_class(**dict( ('section_'+key.replace('-','_'),val)for key,val in self._configparser.items())) 
+    def set_configparser(self):
+        for key_,val in self._configparser.items():
+            keydash='section_'+key_.replace('-','_')
+            for key,subval in val.items():
+                self._configparser[key_][key]=self.config[keydash][key]
+    def __repr__(self) -> str:
+        return __class__.__qualname__+'(configfile_path=%s)'%self._configfile_path
+    #Class and methods
+    def FFT_DC_convergence_test(self):
+        self.set_configparser()
+        DMDdo.FFT_DC_convergence_test.do(self._DMDana_ini)
+    def FFT_spectrum_plot(self):
+        self.set_configparser()
+        DMDdo.FFT_spectrum_plot.do(self._DMDana_ini)
+    def current_plot(self):
+        self.set_configparser()
+        DMDdo.current_plot.do(self._DMDana_ini)
+    def occup_deriv(self):
+        self.set_configparser()
+        DMDdo.occup_deriv.do(self._DMDana_ini)
+    def occup_time(self):
+        self.set_configparser()
+        DMDdo.occup_time.do(self._DMDana_ini)
+    def use_local_config(self):
+        self.configfile_path=self.folder+'/DMDana.ini'
+
+@dataclass
+class occupation_file_class:
+    #Fields
+    path:str
+    data_fs:np.ndarray=field(init=False,repr=False)
+    nstep:int=field(init=False)
+    time_fs:float=field(init=False)
+    #Initialization
+    def __post_init__(self):
+        # nk = 941 nb = 6 nstep = 1 t = 41.341374 tstep = 41.341374 tend = 1240241.225128
+        self.nstep,time_au=DMDparser.read_text_from_file(self.path,['nk =']*2,[9,12],True,[int,float])
+        self.time_fs=time_au/const.fs
+        assert self.nstep!=None, "file %s might be empty"%self.path
+        self.data_fs=np.loadtxt(self.path)
+        self.data_fs[:,0]=self.data_fs[:,0]/const.fs
+@dataclass
+class occupations_class:
+    #Fields
+    folder:str
+    list:List=field(init=False,repr=False)
+    #Initialization
+    def __post_init__(self):
+        self.list=DMDparser.glob_occupation_files(self.folder)
+    #Class and methods
+    def file(self,i):
+        return occupation_file_class(self.list[i])
+from typing import Type
+@dataclass
+class DMD(object):
+    #Fields
+    folder:str
+    param:object=field(init=False,repr=False)
+    occupations:occupations_class=field(init=False,repr=False)
+    analyze:analyze_class=field(init=False,repr=True)
+    #Initialization
+    def __post_init__(self):
+        param_dist=DMDparser.get_DMD_param(self.folder)
+        self.param=param_class(**param_dist)
+        self.occupations=occupations_class(folder=self.folder)
+        self.analyze=analyze_class(folder=self.folder)
+
